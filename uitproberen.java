ea595f14763ec578a50a07ec003557d354646cd6
FelixvL <digitaleservice@gmail.com>
2020-10-02 12:07:46 +0200

    speler met schaakspel opdracht met scan in

--- uitproberen.java
+++ uitproberen.java
@@ -1,54 +1,39 @@
+import java.util.Scanner;
 class Demo{
 	public static void main(String args[]){
-		System.out.println("hoi");
-		int getal = 34;
-		System.out.println(getal);
-		double kommagetal = 55.6;
-		System.out.println(kommagetal);
-		String woord = "woensdag";
-		System.out.println(woord);
 		Speler speler1 = new Speler();
-		Speler speler2 = new Speler();
 		speler1.voornaam = "Johan";
-		System.out.println(speler1.voornaam);
-		System.out.println(speler1);
-		System.out.println(speler1.leeftijd);
-		System.out.println(speler2.voornaam);
+		speler1.voorstellen();
+		Scanner scanner1 = new Scanner(System.in);
+		String test = scanner1.nextLine();
+		speler1.nieuweNaamGeven("Frits"+test);  // argument
 		speler1.voorstellen();
 	}
 
 }
-///  methode functie   ()
+
 class Speler{
 	String voornaam;
 	int leeftijd;
 	void voorstellen(){
 		System.out.println("Hoi ik ben: "+voornaam);
 	}
+	void nieuweNaamGeven(String nieuweNaam){  // parameter
+		System.out.println("vanaf nu heet ik "+nieuweNaam);
+		voornaam = nieuweNaam;
+	}
 }
-
-
-
-
-
 
 
 class Schaakspel{
 	Speler zwart;
 	Speler wit;
+	void starten(){
+
+	}
 }
-
-// Object OOP Class Object
-
-
-// Klasse		kenmerken	allemaal delen 		Voornaam
-// Object		kenmerken	iedereen verschillend	Felix
+// Output   Hoi ik ben Jan en speel met wit
+//		Hoi ik ben Frits en ik speel met zwart
 
 
-// Klasse		Kenmerken
-// Object		Waarde van kenmerken
-
-// Klasse Objecten		Object -> uitgietsel van een klasse
-
-// Klasse		Bouwtekening	Ovenschaal	Idee van Plato
-// Object		Huis		Lasagne		Fysieke verschijning
+// argument parameter
